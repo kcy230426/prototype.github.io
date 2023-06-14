c4481d18003c128ac736c04735d62a382d2a3b41
kcy230426 <132032089+kcy230426@users.noreply.github.com>
2023-06-14 16:44:12 +0900

    커밋
    첫번째 작업 후 업로드

---
+++ js/jQuery.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // 스크롤을 하면
+        if($(window).scrollTop() > 80){
+            // 스크롤의 위치가 80보다 커지면
+            $("body").addClass("scroll");
+            // body에 scroll 클래스를 삽입해라.
+        }else{
+            $("body").removeClass("scroll")
+        };  })
+    $(".nav-link").click(function(){
+        $(".nav-link").removeClass("bolding")
+        $(this).addClass("bolding")
+    })
+})
---
+++ js/java.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('#hd .swiper', {
+    direction: 'vertical',
+    loop: true,
+
+    autoplay: {
+        delay: 3500,
+        disableOnInteraction: false,
+      },
+  });
+  
